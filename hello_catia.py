39feae7d380864379658560f08f14590d72bd80e
Ranjith-Str <ranjith.st03@gmail.com>
2025-09-16 17:27:42 +0530

    Initial commit: Hello CATIA automation script

---
+++ hello_catia.py
@@ -0,0 +1,10 @@
+﻿# hello_catia.py
+import win32com.client as win32
+
+try:
+    catia = win32.Dispatch("Catia.Application")
+    catia.Visible = True
+    doc = catia.Documents.Add("Part")
+    print("Hello CAD Automation! Part created.")
+except Exception as e:
+    print("ERROR connecting to CATIA:", e)
